1bc0fd78181a5dfeabf5fe74a88569f2fa08d902
lucaschej <lchej@campus.ungs.edu.ar>
2024-06-18 13:53:29 -0300

    update

--- Source/config.h
+++ Source/config.h
@@ -6,7 +6,7 @@ const double XMAX = 25; // grid x size
 const double YMAX = 25; // grid y size
 const int NMEANDATA = 10;
 const double fraction_its = 0.3; //Fraction of iterations assumed before equilibrium
-const double prob = 0.001; // Probability of find particles
+//const double prob = 0.001; // Probability of find particles
 
 #endif // CONFIG_H
 
